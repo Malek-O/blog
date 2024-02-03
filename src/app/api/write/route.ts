04f960e7d8557f4d67ac21090b5c4d4484cd0269
Malek-O <lslsls96996@gmail.com>
2024-02-03 21:00:14 +0300

    fix: db insensitive

--- src/app/api/write/route.ts
+++ src/app/api/write/route.ts
@@ -41,6 +41,7 @@ export async function POST(request: NextRequest) {
             where: {
                 tag_name: {
                     equals: cat,
+                    mode: 'insensitive'
                 }
             }
         })
